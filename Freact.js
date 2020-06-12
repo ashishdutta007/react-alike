7d8f0297d27b22dc6109cf749ea8b29fdd686092
ashishdutta007 <ashish.dutta050@gmail.com>
2020-06-13 00:14:59 +0530

    4 files modified

--- Freact.js
+++ Freact.js
@@ -2,6 +2,7 @@ export default class Freact {
   constructor() {}
 
   // generates React element from JSX
+  // returns an object with two properties: type and props
   createElement = (type, props, ...children) => {
     return {
       type: type,
@@ -12,16 +13,28 @@ export default class Freact {
     };
   };
 
-  // render element tree to container
+  // render the element tree to a container
+  // render: is where React changes the DOM
   render = (element, container) => {
-    const dom = document.createElement(elemeny.type);
-    container.appendChild(element);
-  };
-
-  // utility to create DOM node based to el type
-  createDOMElement = elType => {
-    if (typeof elType === "text") {
-      createTextDOMNode(elType);
+    debugger;
+    const { type, props = { children } } = element;
+    const node = document.createElement(type);
+    Object.entries(props).forEach(([key, val]) => {
+      if (key !== "children") {
+        node[key] = val;
+      }
+    });
+    container.appendChild(node);
+    for (child of props.children) {
+      if (typeof child == "object") {
+        this.render(child, node);
+      }
     }
   };
 }
+
+const freact = new Freact();
+
+const element = freact.createElement("h1", { title: "foo" }, "Hello");
+const container = document.getElementById("app");
+freact.render(element, container);
--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-import Freact from "./Freact";
+import "./style.css";
+class Freact {
+  constructor() {}
+
+  // generates React element from JSX
+  // returns an object with two properties: type and props
+  createElement = (type, props, ...children) => {
+    return {
+      type: type,
+      props: {
+        ...props,
+        children
+      }
+    };
+  };
+
+  // render the element tree to a container
+  // render: is where React changes the DOM
+  render = (element, container) => {
+    debugger;
+    const { type, props = { children } } = element;
+    const node = document.createElement(type);
+    Object.entries(props).forEach(([key, val]) => {
+      if (key !== "children") {
+        node[key] = val;
+      }
+    });
+    container.appendChild(node);
+    for (child of props.children) {
+      if (typeof child == "object") {
+        this.render(child, node);
+      }
+    }
+  };
+}
+
 const freact = new Freact();
-
-// const element = <h1 title="foo">Hello</h1>;
-// const container = document.getElementById("root");
-// ReactDOM.render(element, container);
-
 const element = freact.createElement("h1", { title: "foo" }, "Hello");
-const container = document.getElementById("app")
+const container = document.getElementById("app");
+debugger;
 freact.render(element, container);
-
