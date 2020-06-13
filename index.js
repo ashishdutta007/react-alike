84cc223150c53ebe0259e042682d8f915f03ff83
Ashish Dutta <ashish.dutta@medlife.com>
2020-06-13 14:19:04 +0530

    render function

--- index.js
+++ index.js
@@ -1,41 +1,73 @@
-// import "./style.css";
-class Freact {
-  constructor() {}
+const utils = {
+  getAllChildren: children => {
+    return children.map(child => {
+      if (typeof child == "object") return child;
+      else return utils.createTextElement(child);
+    });
+  },
+  createTextElement: text => ({
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }),
+  createDOMnode: type => {
+    if (type === 'TEXT_ELEMENT') {
+      return document.createTextNode();
+    } else {
+      return document.createElement(type);
+    }
+  }
+};
 
+const Freact = {
   // generates React element from JSX
   // returns an object with two properties: type and props
-  createElement = (type, props, ...children) => {
+  createElement: (type, props, ...children) => {
     return {
       type: type,
       props: {
         ...props,
-        children
+        children: utils.getAllChildren(children),
       }
     };
-  };
-
+  },
   // render the element tree to a container
   // render: is where React changes the DOM
-  render = (element, container) => {
-    debugger;
-    const { type, props = { children } } = element;
-    const node = document.createElement(type);
+  // add/update/delete nodes to DOM
+  render: (element, container) => {
+    const { type, props } = element;
+    const node = utils.createDOMnode(type);
+
+    // set all the props to the node except its children
     Object.entries(props).forEach(([key, val]) => {
       if (key !== "children") {
         node[key] = val;
       }
     });
-    container.appendChild(node);
-    for (child of props.children) {
-      if (typeof child == "object") {
-        this.render(child, node);
-      }
-    }
-  };
-}
 
-const freact = new Freact();
-const element = freact.createElement("h1", { title: "foo" }, "Hello");
+    // render all children recursively
+    props.children.forEach(child => render(child, node));
+
+    container.appendChild(node);
+  },
+};
+
+const element = Freact.createElement("div", { id: "foo" },
+  Freact.createElement("a", null, "bar"),
+  Freact.createElement("br")
+);
 const container = document.getElementById("app");
-debugger;
-freact.render(element, container);
+Freact.render(element, container);
+
+
+
+
+// tell babel to use Didact’s createElement instead of React’s?
+// when babel transpiles the JSX it will use the function we define.
+/** @jsx Didact.createElement */
+// <div id="foo">
+// <a>bar</a>
+// <b />
+// </div>
