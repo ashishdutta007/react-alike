ee3936831000a3b0a753d0ee960c8084cda71e77
ashishdutta007 <ashish.dutta050@gmail.com>
2020-06-14 00:19:12 +0530

    1 file modified

--- Freact.js
+++ Freact.js
@@ -31,7 +31,45 @@ function workLoop(deadline) {
 
 // actual rendering of fiber(element) to DOM
 function performUnitWork(fiber) {
-  const node = createDOMnode(fiber);
+  // 1. Create dom node
+  if (fiber.dom == null) {
+    fiber.dom = createDOMnode(fiber);
+  }
+  // 2. Append current fiber dom to parent fiber dom
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  //3. Create new fiber for each children
+  let prevSibling = null;
+  fiber.props.children.map((child, index) => {
+    const newFiber = {
+      dom: null,
+      parent: fiber,
+      props: child.props,
+      type: child.type
+    };
+    //4. Add newFiber to tree as child or sibling
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      // ?????
+      prevSibling.sibling = newFiber;
+    }
+    // ?????
+    prevSibling = newFiber;
+  });
+
+  // 5. Return the nextUnitOfWork
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 let nextUnitOfWork = null;
